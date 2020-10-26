a8c953c2017f978a82a704041f1afd81393aa808
besenio <kbesenio2@yahoo.com>
2020-10-26 02:41:36 -0700

    graph issues fix

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,6 +2,50 @@ import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import numeral from 'numeral';
 
+// const options = {
+//    legend: {
+//       display: false,
+//    },
+//    elements: {
+//       point: {
+//          radius: 0,
+//       }
+//    },
+//    maintainAspectRatio: false,
+//    tooltips: {
+//       mode: 'index',
+//       intersect: false,
+//       callbacks: {
+//          label: function (tooltipItem, data) {
+//             return numeral(tooltipItem.value).format('+0.0');
+//          }
+//       }
+//    },
+//    scales: {
+//       xAxes: [
+//          {
+//             type: 'time',
+//             time: {
+//                format: 'MM/DD/YY',
+//                tooltipFormat: 'll'
+//             }
+//          }
+//       ],
+//       yAxes: [
+//          {
+//             gridLines: {
+//                display: false,
+//             },
+//             ticks: {
+//                callback: function (value, index, values) {
+//                   return numeral(value).format('0a');
+//                }
+//             }
+//          }
+//       ]
+//    }
+// };
+
 const options = {
    legend: {
       display: false,
@@ -9,27 +53,27 @@ const options = {
    elements: {
       point: {
          radius: 0,
-      }
+      },
    },
    maintainAspectRatio: false,
    tooltips: {
-      mode: 'index',
+      mode: "index",
       intersect: false,
       callbacks: {
          label: function (tooltipItem, data) {
-            return numeral(tooltipItem.value).format("+0.0");
-         }
-      }
+            return numeral(tooltipItem.value).format("+0,0");
+         },
+      },
    },
    scales: {
       xAxes: [
          {
-            type: 'time',
+            type: "time",
             time: {
-               format: 'MM/DD/YY',
-               tooltipFormat: 'll'
-            }
-         }
+               format: "MM/DD/YY",
+               tooltipFormat: "ll",
+            },
+         },
       ],
       yAxes: [
          {
@@ -37,69 +81,94 @@ const options = {
                display: false,
             },
             ticks: {
+               // Include a dollar sign in the ticks
                callback: function (value, index, values) {
-                  return numeral(value).format('0a');
-               }
-            }
-         }
-      ]
-   }
+                  return numeral(value).format("0a");
+               },
+            },
+         },
+      ],
+   },
 };
 
-const buildChartData = (data, casesType) => {
-   const chartData = [];
-   let lastDataPoint;
+// const buildChartData = (data, casesType) => {
+//    const chartData = [];
+//    let lastDataPoint;
 
-   for (let date in data.cases) {
-      if (lastDataPoint) {
-         let newDataPoint = {
-            x: date,
-            y: data[casesType][data] - lastDataPoint
-         };
-         chartData.push(newDataPoint);
-      }
-      lastDataPoint = data[casesType][date];
-   }
+//    for (let date in data.cases) {
+//       if (lastDataPoint) {
+//          let newDataPoint = {
+//             x: date,
+//             y: data[casesType][data] - lastDataPoint
+//          };
+//          chartData.push(newDataPoint);
+//       }
+//       lastDataPoint = data[casesType][date];
+//    }
    
-   return chartData;
-}
+//    return chartData;
+// };
 
-function Graph({ casesType }) {
+function Graph() {
    const [data, setData] = useState({});
 
-   useEffect(() => {
-      const fetchData = async () => {
-         await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-            .then(response => {
-               return response.json();
-            })
-            .then(data => {
-               let chartData = buildChartData(data, casesType);
-               setData(chartData);
-         });
-      };
+   const buildChartData = (data, casesType='cases') => {
+      const chartData = [];
+      let lastDataPoint;
 
-      fetchData();
-   }, [casesType])
+      for (let date in data.cases) {
+         if (lastDataPoint) {
+            const newDataPoint = {
+               x: date,
+               y: data[casesType][date] - lastDataPoint
+            }
+            chartData.push(newDataPoint);
+         }
+         lastDataPoint = data[casesType][date];
+      }
+      return chartData
+   }
+
+   useEffect(() => {
+      fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+         .then(response => response.json())
+         .then(data => {
+            const chartData = buildChartData(data, 'cases');
+            console.log("CHARTDATA =>>>", chartData)
+            setData(chartData);
+         });
+   }, []);
+
+   // useEffect(() => {
+   //    const fetchData = async () => {
+   //       await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+   //          .then(response => {
+   //             return response.json();
+   //          })
+   //          .then(data => {
+   //             let chartData = buildChartData(data, "cases");
+   //             setData(chartData);
+   //       });
+   //    };
+
+   //    fetchData();
+   // }, [casesType]);
 
    return (
       <div>
-         {data?.length > 0 && (
-            <Line
-               data = {{
-                  datasets: [
-                     {
-                        backgroundColor: 'rgba(204, 16, 52, 0.5',
-                        borderColor: '#CC1034',
-                        data: data
-                     }
-                  ]
-               }}
-               options={options}
-            />
-         )}
+         <h1>Graph</h1>
+         <Line
+            options={options}
+            data={{
+               datasets: [{
+                  backgroundColor: "rgba(204, 16, 52, 0.5)",
+                  borderColor: "#CC1034",
+                  data: data
+               }]
+            }} 
+         />
       </div>
-   )
+   );
 }
 
 export default Graph;
