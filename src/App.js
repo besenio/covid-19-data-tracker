f8a70f20cadf8b530c19161795d6863bfa12ed8d
besenio <kbesenio2@yahoo.com>
2020-10-25 18:52:15 -0700

    useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+              name: country.country,
+              value: country.countryInfo.iso2
+            }));
+
+            setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>COVID-19 Data Tracker</h1>
+      <div className="app-header">
+        <h1>COVID-19 Data Tracker</h1>
+        <FormControl className="app-dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
