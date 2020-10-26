89f8d0cea9a947c910b40d4df49cce7f5958cb23
besenio <kbesenio2@yahoo.com>
2020-10-26 02:59:31 -0700

    graph rendering

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,50 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import numeral from 'numeral';
 
-// const options = {
-//    legend: {
-//       display: false,
-//    },
-//    elements: {
-//       point: {
-//          radius: 0,
-//       }
-//    },
-//    maintainAspectRatio: false,
-//    tooltips: {
-//       mode: 'index',
-//       intersect: false,
-//       callbacks: {
-//          label: function (tooltipItem, data) {
-//             return numeral(tooltipItem.value).format('+0.0');
-//          }
-//       }
-//    },
-//    scales: {
-//       xAxes: [
-//          {
-//             type: 'time',
-//             time: {
-//                format: 'MM/DD/YY',
-//                tooltipFormat: 'll'
-//             }
-//          }
-//       ],
-//       yAxes: [
-//          {
-//             gridLines: {
-//                display: false,
-//             },
-//             ticks: {
-//                callback: function (value, index, values) {
-//                   return numeral(value).format('0a');
-//                }
-//             }
-//          }
-//       ]
-//    }
-// };
-
 const options = {
    legend: {
       display: false,
@@ -91,82 +47,52 @@ const options = {
    },
 };
 
-// const buildChartData = (data, casesType) => {
-//    const chartData = [];
-//    let lastDataPoint;
+const buildChartData = (data, casesType='cases') => {
+   const chartData = [];
+   let lastDataPoint;
 
-//    for (let date in data.cases) {
-//       if (lastDataPoint) {
-//          let newDataPoint = {
-//             x: date,
-//             y: data[casesType][data] - lastDataPoint
-//          };
-//          chartData.push(newDataPoint);
-//       }
-//       lastDataPoint = data[casesType][date];
-//    }
-   
-//    return chartData;
-// };
+   for (let date in data.cases) {
+      if (lastDataPoint) {
+         const newDataPoint = {
+            x: date,
+            y: data[casesType][date] - lastDataPoint
+         }
+         chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[casesType][date];
+   }
+   return chartData
+}
 
-function Graph() {
+function Graph({ casesType='cases' }) {
    const [data, setData] = useState({});
 
-   const buildChartData = (data, casesType='cases') => {
-      const chartData = [];
-      let lastDataPoint;
-
-      for (let date in data.cases) {
-         if (lastDataPoint) {
-            const newDataPoint = {
-               x: date,
-               y: data[casesType][date] - lastDataPoint
-            }
-            chartData.push(newDataPoint);
-         }
-         lastDataPoint = data[casesType][date];
-      }
-      return chartData
-   }
-
    useEffect(() => {
-      fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-         .then(response => response.json())
-         .then(data => {
-            const chartData = buildChartData(data, 'cases');
-            console.log("CHARTDATA =>>>", chartData)
-            setData(chartData);
-         });
-   }, []);
-
-   // useEffect(() => {
-   //    const fetchData = async () => {
-   //       await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-   //          .then(response => {
-   //             return response.json();
-   //          })
-   //          .then(data => {
-   //             let chartData = buildChartData(data, "cases");
-   //             setData(chartData);
-   //       });
-   //    };
-
-   //    fetchData();
-   // }, [casesType]);
+      const fetchData = async () => {
+         await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+            .then(response => response.json())
+            .then(data => {
+               const chartData = buildChartData(data, 'cases');
+               setData(chartData);
+            });
+         }
+         fetchData();
+      }, [casesType]);
 
    return (
       <div>
          <h1>Graph</h1>
-         <Line
-            options={options}
-            data={{
-               datasets: [{
-                  backgroundColor: "rgba(204, 16, 52, 0.5)",
-                  borderColor: "#CC1034",
-                  data: data
-               }]
-            }} 
-         />
+         {data?.length > 0 && ( //data && data.length
+            <Line
+               options={options}
+               data={{
+                  datasets: [{
+                     backgroundColor: "rgba(204, 16, 52, 0.5)",
+                     data: data
+                  }]
+               }} 
+            />
+         )}
       </div>
    );
 }
